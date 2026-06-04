56970548e7ca4c02ec0aada4bc5b64b530c589b7
muh.farizzi <ezhifarizzi@gmail.com>
2026-06-04 20:44:02 +0800

    feat: add dependency injection with get_it

--- app/lib/core/di/injection.dart
+++ app/lib/core/di/injection.dart
@@ -1,3 +1,30 @@
-library circlestream.lib.core.di.injection;
+library circlestream.core.di.injection;
 
-// TODO: get_it dependency setup
+import 'package:get_it/get_it.dart';
+import '../storage/secure_storage.dart';
+import '../network/api_client.dart';
+import '../../features/auth/data/auth_repository.dart';
+import '../../features/auth/data/auth_repository_impl.dart';
+import '../../features/auth/bloc/auth_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> setupDependencies() async {
+  // Storage
+  sl.registerLazySingleton<SecureStorage>(() => SecureStorage());
+
+  // Network Client
+  sl.registerLazySingleton<ApiClient>(() => ApiClient(secureStorage: sl<SecureStorage>()));
+
+  // Repositories
+  sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(
+        apiClient: sl<ApiClient>(),
+        storage: sl<SecureStorage>(),
+      ));
+
+  // Blocs
+  sl.registerFactory(() => AuthBloc(
+        authRepository: sl<AuthRepository>(),
+        secureStorage: sl<SecureStorage>(),
+      ));
+}
