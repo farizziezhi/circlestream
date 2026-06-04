044bad0d76c3cf64e619ebf59cfda43694ca9d91
muh.farizzi <ezhifarizzi@gmail.com>
2026-06-04 20:34:17 +0800

    feat: add secure storage wrapper

--- app/lib/core/storage/secure_storage.dart
+++ app/lib/core/storage/secure_storage.dart
@@ -1,3 +1,35 @@
-library circlestream.lib.core.storage.secure_storage;
+library circlestream.core.storage.secure_storage;
 
-// TODO: flutter_secure_storage wrapper
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../config/constants.dart';
+
+class SecureStorage {
+  static const _storage = FlutterSecureStorage();
+
+  Future<void> saveTokens({
+    required String accessToken,
+    required String refreshToken,
+  }) async {
+    await Future.wait([
+      _storage.write(key: AppConstants.accessTokenKey, value: accessToken),
+      _storage.write(key: AppConstants.refreshTokenKey, value: refreshToken),
+    ]);
+  }
+
+  Future<String?> getAccessToken() =>
+      _storage.read(key: AppConstants.accessTokenKey);
+
+  Future<String?> getRefreshToken() =>
+      _storage.read(key: AppConstants.refreshTokenKey);
+
+  Future<void> saveAccessToken(String token) =>
+      _storage.write(key: AppConstants.accessTokenKey, value: token);
+
+  Future<String?> getUserId() =>
+      _storage.read(key: AppConstants.userIdKey);
+
+  Future<void> saveUserId(String id) =>
+      _storage.write(key: AppConstants.userIdKey, value: id);
+
+  Future<void> clear() => _storage.deleteAll();
+}
