959713b5c28bee0d11dbcddd11ac4249afb817fb
muh.farizzi <ezhifarizzi@gmail.com>
2026-06-04 20:47:24 +0800

    feat: add placeholder feed screen

--- app/lib/features/feed/screens/feed_screen.dart
+++ app/lib/features/feed/screens/feed_screen.dart
@@ -1,3 +1,19 @@
-library circlestream.lib.features.feed.screens.feed_screen;
+library circlestream.features.feed.screens.feed_screen;
 
-// TODO: Feed post stream screen
+import 'package:flutter/material.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Feed'),
+      ),
+      body: const Center(
+        child: Text('Coming soon'),
+      ),
+    );
+  }
+}
