d006d96b3e0107222f65c59bf55ba03451e30558
muh.farizzi <ezhifarizzi@gmail.com>
2026-06-05 10:43:23 +0800

    feat: add reaction repository

--- app/lib/features/reaction/data/reaction_repository.dart
+++ app/lib/features/reaction/data/reaction_repository.dart
@@ -1,3 +1,31 @@
-library circlestream.lib.features.reaction.data.reaction_repository;
+library circlestream.features.reaction.data.reaction_repository;
 
-// TODO: Reaction repository interface
+import 'package:equatable/equatable.dart';
+
+abstract class ReactionRepository {
+  Future<ReactionResult> addReaction({required int postId, required String emoji});
+  Future<Map<String, int>> getReactions({required int postId});
+}
+
+class ReactionResult extends Equatable {
+  final int postId;
+  final String emoji;
+  final int count;
+
+  const ReactionResult({
+    required this.postId,
+    required this.emoji,
+    required this.count,
+  });
+
+  factory ReactionResult.fromJson(Map<String, dynamic> json) {
+    return ReactionResult(
+      postId: json['post_id'] as int,
+      emoji: json['emoji'] as String,
+      count: json['count'] as int,
+    );
+  }
+
+  @override
+  List<Object?> get props => [postId, emoji, count];
+}
--- app/lib/features/reaction/data/reaction_repository_impl.dart
+++ app/lib/features/reaction/data/reaction_repository_impl.dart
@@ -1,3 +1,28 @@
-library circlestream.lib.features.reaction.data.reaction_repository_impl;
+library circlestream.features.reaction.data.reaction_repository_impl;
 
-// TODO: Reaction repository implementation
+import '../../../core/network/api_client.dart';
+import '../../../core/network/endpoints.dart';
+import 'reaction_repository.dart';
+
+class ReactionRepositoryImpl implements ReactionRepository {
+  final ApiClient _apiClient;
+
+  ReactionRepositoryImpl({required ApiClient apiClient}) : _apiClient = apiClient;
+
+  @override
+  Future<ReactionResult> addReaction({required int postId, required String emoji}) async {
+    final response = await _apiClient.post(
+      Endpoints.postReactions(postId),
+      data: {'emoji': emoji},
+    );
+    return ReactionResult.fromJson(response.data as Map<String, dynamic>);
+  }
+
+  @override
+  Future<Map<String, int>> getReactions({required int postId}) async {
+    final response = await _apiClient.get(Endpoints.postReactions(postId));
+    final data = response.data as Map<String, dynamic>;
+    final reactionsData = data['reactions'] as Map<String, dynamic>? ?? {};
+    return reactionsData.map((key, value) => MapEntry(key, value as int));
+  }
+}
